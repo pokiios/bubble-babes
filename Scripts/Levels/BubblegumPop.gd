75abb2274cad1b8f18d7c80e0e9a8ef3a55a3f7f
Leah McDermott <2100571@uad.ac.uk>
2025-01-25 18:34:54 +0000

    forgot to save
    fixed my game again

--- Scripts/Levels/BubblegumPop.gd
+++ Scripts/Levels/BubblegumPop.gd
@@ -8,3 +8,8 @@ func _on_level_timer_time_done() -> void:
 		$AnimationPlayer.play("fade_gray")
 	else:
 		print("winner winner chicken dinner :D")
+
+func _on_audio_stream_record_level_complete() -> void:
+	level_completed = true
+	$Background.texture = load("res://Assets/Bubblegum Pop/bubblegum-pop-background-complete.png")
+	$Bubble.visible = false
