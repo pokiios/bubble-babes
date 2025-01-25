b625df558eca79605f81bcb96057adeec982cf7d
Leah McDermott <2100571@uad.ac.uk>
2025-01-25 21:41:15 +0000

    Transitions finished
    they use temp numbers, this is very important, also it calls itself, also important

---
+++ Scripts/Levels/TransitionNeutral.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var instructions_array : Array = ["Tap those Raps!", "Blow up that Gum!", "Mint that Drink!", "Shake the Cans!", "Pop the Suds!", "Vent that Anger!"]
+var lives_position_array : Array = []
+var lives_array : Array = []
+signal transition_finished
+
+func transition_init(random_int : int, lives : int) -> void:
+	$CanvasLayer/MarginContainer/Label.text = instructions_array[random_int]
+
+	for i in lives:
+		var temp = Sprite2D.new()
+		temp.texture = load("res://Sprites/Transitions/heartlife sticker.png")
+		temp.scale = Vector2(.75, .75)
+		temp.position = lives_position_array[i].position
+		lives_array.push_back(temp)
+		add_child(lives_array.back())
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker1)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker2)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker3)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker4)
+	
+	transition_init(2,2)
+	$Timer.start()
+	$AnimationPlayer.play("yapping")
+	
+
+
+func _on_timer_timeout() -> void:
+	# go to game :)
+	transition_finished.emit()
---
+++ Scripts/Levels/TransitionsLose.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var lives_position_array : Array = []
+var lives_array : Array = []
+signal transition_finished
+
+func transition_init(lives, score) -> void:
+	for i in lives:
+		var temp = Sprite2D.new()
+		temp.texture = load("res://Sprites/Transitions/heartlife sticker.png")
+		temp.scale = Vector2(.75, .75)
+		temp.position = lives_position_array[i].position
+		lives_array.push_back(temp)
+		add_child(lives_array.back())
+		
+	$CanvasLayer/MarginContainer/VBoxContainer/Label.text = "Life Lost!"
+	$CanvasLayer/MarginContainer/VBoxContainer/Score.text = "Score:" + str(score)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker1)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker2)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker3)
+	lives_position_array.push_back($CanvasLayer/MarginContainer/Marker4)
+	
+	transition_init(2, 5)
+	$Timer.start()
+	$AnimationPlayer.play("crying")
+	
+
+func _on_timer_timeout() -> void:
+	# go to game :)
+	transition_finished.emit()
